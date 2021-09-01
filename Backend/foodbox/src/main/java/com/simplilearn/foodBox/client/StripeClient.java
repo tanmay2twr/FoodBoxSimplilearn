1400842a961f64b305a0df6d6ed5d58cbee2f433
Sudhindra <sudhindra.simplilearn@gmail.com>
2021-09-01 23:21:00 +0530

    Move final images inside Project Structure and add Payement Gateway

---
+++ Backend/foodbox/src/main/java/com/simplilearn/foodBox/client/StripeClient.java
@@ -0,0 +1,46 @@
+package com.simplilearn.foodBox.client;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.stripe.Stripe;
+import com.stripe.model.Charge;
+import com.stripe.model.Customer;
+@Component
+public class StripeClient {
+    @Autowired
+    StripeClient() {
+        Stripe.apiKey = "sk_test_51JUy66SCFw90Gwcy8euzHgdccXsQJETZnaHsy2sgRTnooAkn3exqP3xSL4Hw0dnWamT3ORAIlvJJ7Q9q9tpqz0HV00EDiwiIyW";
+    }
+    public Customer createCustomer(String token, String email) throws Exception {
+        Map<String, Object> customerParams = new HashMap<String, Object>();
+        customerParams.put("email", email);
+        customerParams.put("source", token);
+        return Customer.create(customerParams);
+    }
+    private Customer getCustomer(String id) throws Exception {
+        return Customer.retrieve(id);
+    }
+
+	public Charge chargeNewCard(String token, double amount) throws Exception {
+        Map<String, Object> chargeParams = new HashMap<String, Object>();
+        chargeParams.put("amount", (int)(amount * 100));
+        chargeParams.put("currency", "INR");
+        chargeParams.put("source", token);
+        Charge charge = Charge.create(chargeParams);
+        return charge;
+    }
+    public Charge chargeCustomerCard(String customerId, int amount) throws Exception {
+        String sourceCard = getCustomer(customerId).getDefaultSource();
+        Map<String, Object> chargeParams = new HashMap<String, Object>();
+        chargeParams.put("amount", amount);
+        chargeParams.put("currency", "INR");
+        chargeParams.put("customer", customerId);
+        chargeParams.put("source", sourceCard);
+        Charge charge = Charge.create(chargeParams);
+        return charge;
+    }
+}
